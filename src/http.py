5813f5160d39be6486b03f43711a4a529e4b3e7c
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2019-02-19 22:39:19 +0100

    added request for reading file

--- src/http.py
+++ src/http.py
@@ -1,5 +1,5 @@
 
-import http.client
+import http.client as http
 import logging, os
 
 
@@ -14,7 +14,7 @@ class Http:
 
     def __init__(self, host_info):
         #print("host_info" + str(host_info))
-        self.conn = http.client.HTTPConnection(str(host_info["host"]), str(host_info["port"]))
+        self.conn = http.HTTPConnection(str(host_info["host"]), str(host_info["port"]))
         self.payload=""
         self.headers=""
         self.request=""
@@ -33,6 +33,19 @@ class Http:
         res = self.conn.getresponse()
         data = res.read()
         data=ast.literal_eval(data.decode("utf-8"))
+        return data
+
+    def send_request_model_read(self, request, endpoint, payload, headers):
+        self.payload=payload
+        self.header=headers
+        self.request=request
+        self.endpoint= endpoint
+
+        logger.debug("Sending request ")
+        self.conn.request(self.request,self.endpoint, self.payload, self.header)
+
+        res = self.conn.getresponse()
+        data = res.read()
         return data
 
     def send_request_add(self, request, endpoint, payload, headers):
